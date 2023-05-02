61dd83b09bc240ee5aa5d5d2a2446c3ae7e171f3
dezhest <dezhest@yandex.ru>
2023-05-02 18:11:39 +0400

    Initial Commit

---
+++ NewsApi/NewsApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsApiApp.swift
+//  NewsApi
+//
+//  Created by Denis Zhesterev on 02.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct NewsApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
